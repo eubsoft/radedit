146e68c4ebe5b4df90e15b0d3ab3362b48bd5450
Sam Eubank <sameubank@gmail.com>
2014-01-24 08:24:35 -0800

    Using Jymin as a dependency

--- public/jymin/dates.js
+++
@@ -1,7 +0,0 @@
-/**
- * Get Unix epoch seconds from a date.
- */
-var getTime = function(date) {
-	date = date || new Date();
-	return date.getTime();
-};
--- public/jymin/dollar.js
+++
@@ -1,2 +0,0 @@
-var $ = getElement;
-var $$ = getElementsByTagAndClass;
