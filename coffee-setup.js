900cfb4cabb4fbb3f327cdd1bb4f0586ff800f43
Sam Eubank <sameubank@gmail.com>
2014-02-24 08:48:03 -0800

    Added coffee-cache for issue #23

--- coffee-setup.js
+++ coffee-setup.js
@@ -2,4 +2,10 @@
 var coffee = require('coffee-script');
 
 // If on v1.7+, register .coffee as a require-able extension.
-if (coffee.register) coffee.register();
+if (coffee.register) {
+	coffee.register();
+}
+
+// Cache compiled CoffeeScript to speed startup.
+var cache = require('coffee-cache');
+cache.setCacheDir('.cache');
